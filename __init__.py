113c7c93084f04176ffd25f3369cbfa3a048dec2
tommy luo <tommy@tommydeMacBook-Air.local>
2015-07-15 18:32:59 +0800

    add flask-debugtoolbar

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 app = Flask(__name__)
 #app.config.from_object('config')
 db=SQLAlchemy(app)
+toolbar = DebugToolbarExtension(app)
 app.config['SECRET_KEY']='you-will-never-guess'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost:3306/test?charset=utf8'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
