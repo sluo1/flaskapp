64aacfdc31a5be4974477173c4652dd07f1f4a89
tommy luo <tommy@tommydeMacBook-Air.local>
2015-07-16 19:39:45 +0800

    0715master

--- forms.py
+++ forms.py
@@ -1,14 +1,16 @@
 from wtforms import Form,PasswordField,validators
 from wtforms import StringField,BooleanField,TextAreaField,TextField
 from wtforms.validators import DataRequired,Length,Required
+from datetime import datetime
 
-
-class AddForm(Form):
+class AddUserForm(Form):
     username=StringField('username',[validators.Length(min=0, max=100)])
     email=StringField('email',[validators.Length(min=0, max=100)])
     password=StringField('password',[validators.Length(min=0, max=100)])
 
-class EditForm(Form):
-    username=StringField('username',[validators.Length(min=0, max=100)])
-    email=StringField('email',[validators.Length(min=0, max=100)])
-    password=StringField('password',[validators.Length(min=0, max=100)])
+
+class AddMemoForm(Form):
+    username=StringField('username',[validators.Length(min=0,max=100)])
+    event=TextAreaField('event',[validators.Length(min=0,max=300)])
+    create_time=datetime.utcnow()
+    update_time=datetime.utcnow()
--- views.py
+++ views.py
@@ -2,8 +2,9 @@ __author__ = 'tommy'
 from __init__ import app,db
 from flask import render_template,flash,redirect,session,url_for,request,g
 import models
-from forms import AddForm,EditForm
+from forms import AddUserForm,AddMemoForm
 from models import User,Memo
+from datetime import datetime
 
 
 @app.route('/')
@@ -21,26 +22,40 @@ def memo():
     events=models.Memo.query.all()
     return render_template('memo.html',events=events)
 
-@app.route('/add',methods=['GET','POST'])
-def add():
-    form=AddForm(request.form)
+@app.route('/adduser',methods=['GET','POST'])
+def adduser():
+    form=AddUserForm(request.form)
     #users=models.User.query.all()
     if request.method == 'POST' and form.validate():
-        u=User(username=form.username.data,email=form.email.data,password=form.password.data)
+        u=User(username=request.form['username'],email=request.form['email'],password=form.password.data)
         db.session.add(u)
         db.session.commit()
-        flash("your changes have been saved.")
+        flash("user has been added.")
         return redirect(url_for('user'))
 
-    return render_template('add.html',form=form)
+    return render_template('adduser.html',form=form)
+
+@app.route('/addmemo',methods=['GET','POST'])
+def addmemo():
+    form=AddMemoForm(request.form)
+    if request.method=='POST':
+
+        m=Memo(username=models.User.query.first(),event=form.event.data,create_time=datetime.utcnow(),update_time=datetime.utcnow())
+        db.session.add(m)
+        db.session.commit()
+        flash("your memo have been saved")
+        return redirect(url_for('memo'))
+    return render_template('addmemo.html',form=form)
 
 
-@app.route("/testform")
+@app.route("/testform",methods=['GET','POST'])
 def testform():
-    return render_template('testform.html')
+    # if request.method=="POST":
+    #     namevalue= request.form['name']
+    #     passwordvalue= request.form['password']
+    form=request.form
+    return render_template('testform.html',form=form)
 
 
-# @app.route('/edituser',methods=['GET','POST'])
-# def edituser():
-#     username=models.User.username
+
 
